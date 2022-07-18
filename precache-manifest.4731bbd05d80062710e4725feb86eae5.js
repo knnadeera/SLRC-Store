38e2512a0ddad3cbed4648fe91450782d05a81d4
knnadeera <knnadeera1@gmail.com>
2022-07-18 13:55:08 +0530

    Updates

---
+++ precache-manifest.4731bbd05d80062710e4725feb86eae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb2e52c8102b3597e3ac",
+    "url": "/SLRC-Store/static/css/main.e79c9823.chunk.css"
+  },
+  {
+    "revision": "bb2e52c8102b3597e3ac",
+    "url": "/SLRC-Store/static/js/main.bb2e52c8.chunk.js"
+  },
+  {
+    "revision": "703c63741c15e8451c41",
+    "url": "/SLRC-Store/static/js/1.703c6374.chunk.js"
+  },
+  {
+    "revision": "f3fddd4c6fb3c5696e90",
+    "url": "/SLRC-Store/static/js/runtime~main.f3fddd4c.js"
+  },
+  {
+    "revision": "2f7903bb98b08e8ae798d7eedfa23d32",
+    "url": "/SLRC-Store/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/SLRC-Store/precache-manifest.4731bbd05d80062710e4725feb86eae5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/SLRC-Store/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
