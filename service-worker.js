00492274da6ee2fe4ae18c73a69d3f0d78ede002
knnadeera <knnadeera1@gmail.com>
2022-09-22 20:51:43 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/SLRC-Store/precache-manifest.e3f0f100ff8bdb13641036eb6dcbeda4.js"
+  "/SLRC-Store/precache-manifest.2a812c7b488f2161b082b0ebfc8181b1.js"
 );
 
 workbox.clientsClaim();
