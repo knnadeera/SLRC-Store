e125707d2a07c5afaa6b29e9698c3c13153a7e45
knnadeera <knnadeera1@gmail.com>
2022-09-23 13:11:40 +0530

    Updates

---
+++ precache-manifest.ce77d6a5736d5873c468a510d8c08350.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "d017d273bee204df6b09",
+    "url": "/SLRC-Store/static/css/0.b5efd625.chunk.css"
+  },
+  {
+    "revision": "d017d273bee204df6b09",
+    "url": "/SLRC-Store/static/js/0.d017d273.chunk.js"
+  },
+  {
+    "revision": "0a91a4ca5a1a20f7ac9c",
+    "url": "/SLRC-Store/static/css/main.0acbc21c.chunk.css"
+  },
+  {
+    "revision": "0a91a4ca5a1a20f7ac9c",
+    "url": "/SLRC-Store/static/js/main.0a91a4ca.chunk.js"
+  },
+  {
+    "revision": "4edbb7d8f9530e616aac",
+    "url": "/SLRC-Store/static/css/2.5c31bb3f.chunk.css"
+  },
+  {
+    "revision": "4edbb7d8f9530e616aac",
+    "url": "/SLRC-Store/static/js/2.4edbb7d8.chunk.js"
+  },
+  {
+    "revision": "fcb18b2fd9cfbdfca30d",
+    "url": "/SLRC-Store/static/css/3.f5b03176.chunk.css"
+  },
+  {
+    "revision": "fcb18b2fd9cfbdfca30d",
+    "url": "/SLRC-Store/static/js/3.fcb18b2f.chunk.js"
+  },
+  {
+    "revision": "05c34ea3357ec6f7618d",
+    "url": "/SLRC-Store/static/css/4.f34003f7.chunk.css"
+  },
+  {
+    "revision": "05c34ea3357ec6f7618d",
+    "url": "/SLRC-Store/static/js/4.05c34ea3.chunk.js"
+  },
+  {
+    "revision": "31b3acb18d913b6e7c22",
+    "url": "/SLRC-Store/static/css/5.724b9ae9.chunk.css"
+  },
+  {
+    "revision": "31b3acb18d913b6e7c22",
+    "url": "/SLRC-Store/static/js/5.31b3acb1.chunk.js"
+  },
+  {
+    "revision": "fa8aed439548f38c7d4b",
+    "url": "/SLRC-Store/static/css/6.42d37437.chunk.css"
+  },
+  {
+    "revision": "fa8aed439548f38c7d4b",
+    "url": "/SLRC-Store/static/js/6.fa8aed43.chunk.js"
+  },
+  {
+    "revision": "5d9666dc8713419aafe3",
+    "url": "/SLRC-Store/static/css/7.e93111dd.chunk.css"
+  },
+  {
+    "revision": "5d9666dc8713419aafe3",
+    "url": "/SLRC-Store/static/js/7.5d9666dc.chunk.js"
+  },
+  {
+    "revision": "20b76b94862bffbfc95f",
+    "url": "/SLRC-Store/static/css/8.90110411.chunk.css"
+  },
+  {
+    "revision": "20b76b94862bffbfc95f",
+    "url": "/SLRC-Store/static/js/8.20b76b94.chunk.js"
+  },
+  {
+    "revision": "79ad01c3539880ee5ebb",
+    "url": "/SLRC-Store/static/js/9.79ad01c3.chunk.js"
+  },
+  {
+    "revision": "71b378e8b6aafdcf76ef",
+    "url": "/SLRC-Store/static/js/runtime~main.71b378e8.js"
+  },
+  {
+    "revision": "c0a29de3ff28e29cbc2578b279b01207",
+    "url": "/SLRC-Store/index.html"
+  }
+];
